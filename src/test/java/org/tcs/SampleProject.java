b5eac06c96b52f703f3dadb051fe4324dab53352
Anusuyaashwathy <anuanushya194@gmail.com>
2023-04-29 19:48:55 +0530

    First time push

--- src/test/java/org/tcs/SampleProject.java
+++ src/test/java/org/tcs/SampleProject.java
@@ -1,5 +1,22 @@
 package org.tcs;
 
 public class SampleProject {
+	
+private void method1() {
+	System.out.println("1");
+
+}
+private void method2() {
+	System.out.println("2");
+
+}
+private void method3() {
+	System.out.println("3");
+
+}
+private void method4() {
+	System.out.println("4");
+
+}
 
 }
